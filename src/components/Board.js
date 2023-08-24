0983a164331cb0579e64ca23e5a52e52b359c179
박혜민 <hyeminpak@gmail.com>
2023-08-24 16:16:59 +0900

    승자 결정하기

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,9 +5,42 @@ import "./Board.css";
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
-  const status = `Next player ${xIsNext ? "X" : "O"}`;
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+  let status;
+  const winner = calculateWinner(squares);
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = `Next Player: ${xIsNext ? "X" : "O"}`;
+  }
   const handleClick = (i) => {
     const newSquares = [...squares]; //복사
+    console.log(calculateWinner(newSquares));
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
     newSquares[i] = xIsNext ? "X" : "O";
     setSquares(newSquares);
     setXIsNext((prev) => !prev);
@@ -16,6 +49,7 @@ const Board = () => {
   const renderSquare = (i) => {
     return <Square onClick={() => handleClick(i)} value={squares[i]} />;
   };
+
   return (
     <div>
       {status}
