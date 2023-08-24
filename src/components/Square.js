084e5370831dd6252bfcd0c6622279f3ee6afa13
박혜민 <hyeminpak@gmail.com>
2023-08-24 13:16:47 +0900

    사용자와 상호작용하는 컴포넌트 만들기

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Square.css";
 export default class Square extends React.Component {
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button onClick={() => console.log("click")} className="square">
+        {this.props.value}
+      </button>
+    );
   }
 }
